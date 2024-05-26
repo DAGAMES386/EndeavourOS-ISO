406bf22a4ce7f281ef797f8ac8fba18114d14a48
budge <budgethecat@proton.me>
2024-05-25 19:20:38 -0700

    update for my own tastes :p

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf "work" "out"
-rm airootfs/root/packages/*.pkg.tar.zst
-rm airootfs/root/packages/*.pkg.tar.zst.sig
+# rm airootfs/root/packages/*.pkg.tar.zst
+# rm airootfs/root/packages/*.pkg.tar.zst.sig
 rm -rf airootfs/root/endeavouros-skel-liveuser/pkg
 rm airootfs/root/endeavouros-wallpaper.png
 rm airootfs/root/endeavouros-skel-liveuser/*.pkg.tar.zst
